51e2e58b36d082be35c58e425242ad3344dc5bb7
Nayani De Alwis <nayani.dealwis@acc.co.nz>
2021-03-20 19:16:40 +1300

    rebase added line

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,9 +1,8 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
-        System.out.println("Change 1- feature Branch")
-        System.out.println("Change 3- feature Branch");
-        System.out.println("Change 2 - feature Branch");
-        System.out.println("Change 4- feature Branch");
+        System.out.println("Change 1- feature Branch");
+        System.out.println("rebase test");
+
     }
 }
